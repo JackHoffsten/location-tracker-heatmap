061300de3655c8be98b2e46d9d66f2ab5d6e68f2
Jack Hoffsten <jack.hoffsten@hotmail.se>
2020-03-19 15:13:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Starting server at ${port}...`);
+});
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/all', (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      res.end();
+      return;
+    }
+
+    res.json(data);
+  });
+});
+
+app.post('/location', (req, res) => {
+    const data = req.body;
+    const timestamp = Date.now();
+
+    data.timestamp  = timestamp;
+
+    database.insert(data);
+
+    res.json(data);
+});
