41bb05fd051016184dfc61752d2977cfcc740bf4
Jack Hoffsten <jack.hoffsten@hotmail.se>
2020-03-23 14:00:33 +0100

    aaa

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.post('/location', (req, res) => {
 });
 
 const httpsOptions = {
-  key: fs.readFileSync('location-tracker-heatmap/ssl/key.pem'),
-  cert: fs.readFileSync('location-tracker-heatmap/ssl/cert.pem'),
+  key: fs.readFileSync('app/ssl/key.pem'),
+  cert: fs.readFileSync('app/ssl/cert.pem'),
 };
 
 https.createServer(httpsOptions, app).listen(port, () => {
